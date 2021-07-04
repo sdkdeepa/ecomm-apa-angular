79adb9ded92cf98dfcad2297bace0a1bea993d5d
Deepa Subramanian <deepa94086@gmail.com>
2021-07-03 21:27:21 -0700

    contact page added details

--- angular/buy-sell-app/src/app/app.module.ts
+++ angular/buy-sell-app/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -23,7 +25,8 @@ import { MyListingsPageComponent } from './my-listings-page/my-listings-page.com
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
