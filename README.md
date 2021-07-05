2da1eda8afbcfd6d2e2750c089ba7df962823c16
Deepa Subramanian <deepa94086@gmail.com>
2021-07-05 02:39:25 -0700

    added update and Delete route

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# ecomm-apa-angular
+# Ecommerce listing site  
+    Built a Full Stack ecommerce listing application using Angular, TypeScript, Javascript, Hapi, MySQL, Firebase, Google Cloud CLI.
---
+++ angular/buy-and-sell-backend/src/routes/deleteListing.js
@@ -0,0 +1,15 @@
+import { db } from '../database';
+
+export const deleteListingRoute = {
+    method: 'DELETE',
+    path: '/api/listings/{id}',
+    handler: async (req, h) => {
+        const id = req.params.id
+        // to increment the number of views to a particular id with the above url
+        await db.query(
+            'DELETE FROM listings WHERE id=?',
+            [id],
+        )
+        return {message: 'Sucess'}
+    }
+}
